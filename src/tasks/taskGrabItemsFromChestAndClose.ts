63596b37d8b27ee3150279b202483699463194a4
autowert66 <72566626+autowert66@users.noreply.github.com>
2023-09-24 18:09:37 +0200

    🥴 use chest position instead of chest info in task

--- src/tasks/taskGrabItemsFromChestAndClose.ts
+++ src/tasks/taskGrabItemsFromChestAndClose.ts
@@ -1,29 +1,19 @@
 import { TaskList } from './taskList';
-import type { Chest as ChestInfo } from '../../config';
-
-import { Vec3 } from 'vec3';
+import type { Vec3 } from 'vec3';
 
 import { TaskOpenChest } from './taskOpenChest';
 import { TaskGrabItemsFromChest } from './taskGrabItemsFromChest';
 import { TaskCloseChest } from './taskCloseChest';
 
 export class TaskGrabItemsFromChestAndClose extends TaskList {
-  protected chestInfo: ChestInfo;
+  protected chestPosition: Vec3;
   protected amount;
 
-  protected chestPosition: Vec3;
-
-  constructor(chestInfo: ChestInfo, amount = 1) {
+  constructor(chestPosition: Vec3, amount = 1) {
     super([]);
 
-    this.chestInfo = chestInfo;
+    this.chestPosition = chestPosition;
     this.amount = amount;
-
-    this.chestPosition = new Vec3(
-      this.chestInfo.position.x,
-      this.chestInfo.position.y,
-      this.chestInfo.position.z
-    );
 
     this.tasks.push(
       new TaskOpenChest(this.chestPosition),
