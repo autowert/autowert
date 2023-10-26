af5229ebe18aceeeb36fbbf5e7d89a73e2514c9b
autowert66 <72566626+autowert66@users.noreply.github.com>
2023-10-26 13:53:15 +0200

    🛠️ add utility functions to useWritableBookPlugin

--- src/plugins/useWritableBookPlugin.ts
+++ src/plugins/useWritableBookPlugin.ts
@@ -1,11 +1,23 @@
 import type { Plugin as BotPlugin } from 'mineflayer';
 
 export const useWritableBookPlugin: BotPlugin = (bot) => {
-  bot.useWritableBook = async (pages, title) => {
+  bot.findWritableBookSlot = () => {
+    const slot = bot.inventory.slots.findIndex(item => item && item.name === 'writable_book');
+    return slot === -1 ? null : slot;
+  }
+  bot.hasWritableBookInInventory = () => {
+    const slot = bot.findWritableBookSlot;
+    return slot !== null;
+  }
+
+  bot.useWritableBook = async (pages, title, options) => {
     const shouldSign = title !== null;
 
-    const writableBookSlot = bot.inventory.slots.findIndex(item => item && item.name === 'writable_book');
-    if (writableBookSlot === -1) throw new Error('no writable book found');
+    const writableBookSlot =
+      options?.slot !== undefined
+        ? options.slot
+        : bot.findWritableBookSlot();
+    if (writableBookSlot === null) throw new Error('no writable book found');
 
     if (shouldSign) {
       // bot.signBook( slot, pages, author, title )
@@ -14,13 +26,21 @@ export const useWritableBookPlugin: BotPlugin = (bot) => {
       await bot.writeBook(writableBookSlot, pages);
     }
 
-    return writableBookSlot;
+    if (options?.drop) {
+      bot.windowInteractions.dropItemFromSlot(writableBookSlot);
+    }
   };
 };
 
+type UseWritableBookOptions = {
+  slot: number;
+  drop: boolean;
+};
 declare module 'mineflayer' {
   interface Bot {
-    useWritableBook: (pages: string[], title: string | null) => Promise<number>;
+    findWritableBookSlot: () => number | null;
+    hasWritableBookInInventory: () => boolean;
+    useWritableBook: (pages: string[], title: string | null, options?: Partial<UseWritableBookOptions>) => Promise<void>;
   }
 }
 
