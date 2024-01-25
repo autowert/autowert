346a940437473acb88109aa22eb4ab7dce97b7ec
autowert66 <72566626+autowert66@users.noreply.github.com>
2024-01-25 20:57:03 +0100

    🧹 move tpy logic to tpy plugin

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,7 @@ import { advertisingPlugin } from './plugins/advertisingPlugin';
 import { TaskGetWritableBook } from './tasks/items/taskGetWritableBook';
 import { commandHandlerPlugin } from './plugins/commandHandlerPlugin';
 import { statisticsPlugin } from './plugins/statisticsPlugin';
+import { tpyPlugin } from './plugins/tpyPlugin';
 
 const botOptions: BotOptions = {
   username: 'autowert',
@@ -58,6 +59,7 @@ const botOptions: BotOptions = {
     advertisingPlugin,
     commandHandlerPlugin,
     statisticsPlugin,
+    tpyPlugin,
   },
 
   logOptions: {
@@ -83,46 +85,6 @@ function createBot() {
   const checkBotUpInterval = setInterval(checkBotUp, 60 * 1000);
   bot.once('end', () => {
     clearInterval(checkBotUpInterval);
-  });
-
-  bot.on('incomingTPrequest', async (from) => {
-    // TODO: USE UUIDS (VERY IMPORTANT)
-    if (from === 'Manue__l') bot.chat('/tpy Manue__l');
-    /* else if (from === 'GoogleComStuff') {
-      if (!bot.players['Manue__l']) {
-        console.log(from, 'wants to tp, but Manue__l is offline');
-
-        bot.chat('/w GoogleComStuff You may not teleport while Manue__l is offline.');
-        return;
-      }
-
-      bot.chat('/w Manue__l GoogleComStuff would like to teleport to autowert.');
-
-      let resolveSent: () => void;
-      let sentPromise = new Promise<void>((resolve) => { resolveSent = resolve; });
-      let chatDisabled = false;
-
-      const onMessageListener: mineflayer.BotEvents['messagestr'] = (message) => {
-        if (/^To (Manue__l): /i.test(message)) resolveSent();
-        if (/^(Manue__l)'s chat is disabled\.$/i.test(message)) chatDisabled = true;
-      };
-      bot.on('messagestr', onMessageListener);
-
-      await sentPromise;
-      await sleep(50);
-
-      console.log(from, 'wants to tp', 'Manue__l is ' + (chatDisabled ? 'afk' : 'not afk'));
-      bot.off('messagestr', onMessageListener);
-
-      if (chatDisabled) {
-        // Manue__l is afk
-        bot.chat('/w GoogleComStuff You may not teleport while Manue__l is afk.');
-        return;
-      }
-
-      bot.chat('/tpy GoogleComStuff');
-    } */
-    else if (from === 'GoogleComStuff') bot.chat('/tpy GoogleComStuff');
   });
 
   bot.on('death', () => {
