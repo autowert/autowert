3daf902dd9d7546aa6d58b1cde69971b4866f534
autowert66 <72566626+autowert66@users.noreply.github.com>
2024-02-25 10:48:40 +0100

    👟 speed up opkit

--- src/commands/opkitCommand.ts
+++ src/commands/opkitCommand.ts
@@ -34,15 +34,36 @@ export const opKitCommand = new TPCommand({
           if (!chestBlock) return;
 
           const chest = await bot.openChest(chestBlock);
-          for (let slot = 0; slot < chest.inventoryStart; slot++) {
-            try {
-              await bot.windowInteractions.shiftLeftClick(slot);
-            } catch (err) {
-              console.log('transaction failed');
-            }
-            await sleep(20);
+
+          let chestItemCount = 0;
+          let inventoryFreeCount = 0;
+
+          for (let slotId = 0; slotId < chest.slots.length; slotId++) {
+            const slot = chest.slots[slotId];
+
+            if (slotId < chest.inventoryStart && slot) chestItemCount++;
+            if (slotId >= chest.inventoryStart && slotId <= chest.inventoryEnd && !slot) inventoryFreeCount++;
           }
 
+          console.log('opkit: found %d items in chest and %d free slots in inventory', chestItemCount, inventoryFreeCount);
+
+          for (let slotId = 0; slotId < chest.inventoryStart; slotId++) {
+            const slot = chest.slots[slotId];
+            if (!slot) continue;
+
+            bot.windowInteractions.shiftLeftClick(slotId)
+
+            chestItemCount--;
+            inventoryFreeCount--;
+
+            if (!chestItemCount || !inventoryFreeCount) break;
+
+            // await sleep(25);
+          }
+
+          console.log('opkit: after moving %d items in chest and %d free slots in inventory', chestItemCount, inventoryFreeCount);
+
+          await sleep(25);
           chest.close();
         }),
       ]),
