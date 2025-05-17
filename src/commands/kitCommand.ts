c7c35698f8e23ad26f4e94bc2dec7a3f091d7874
Gabriel <72566626+autowert66@users.noreply.github.com>
2025-05-17 12:33:49 +0200

    🎯 add target option to kit command

--- src/commands/kitCommand.ts
+++ src/commands/kitCommand.ts
@@ -3,16 +3,34 @@ import { TPCommand } from './TPCommand';
 export const kitCommand = new TPCommand({
   name: 'kit',
   description: 'Teleports to you with a kit.',
-  usage: '[kitName]',
+  usage: '[kitName] [--to=<username>]',
   aliases: ['rekit'],
-  
+
   prefixOverwrite: /.*/,
 
-  execute: async ({ bot, invokerUsername, args }) => {
+  execute: async ({ bot, invokerUsername, args, flags }) => {
     const type = args[0]?.toLowerCase();
+
+    let target = invokerUsername;
+    if ('to' in flags) {
+      if (invokerUsername !== 'Manue__l')
+        return { success: false };
+
+      const targetUsername = flags.to.toString().toLowerCase();
+      const actualUsername = Object.keys(bot.players).find(username => username.toLowerCase() === targetUsername);
+
+      if (!actualUsername) {
+        bot.chat(`/w ${invokerUsername} Target not found`);
+        return { success: false };
+      }
+
+      console.log(`kit command invoked by ${invokerUsername} with target ${targetUsername}`);
+      target = actualUsername;
+    }
+
     const { success } = await bot.kitStore.getKit(invokerUsername, type);
 
     if (!success) return { success: false };
-    return {}; // TODO: return beforeTPTask 
+    return { targetOverwrite: target }; // TODO: return beforeTPTask 
   },
 });
