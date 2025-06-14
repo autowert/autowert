493572d856e61db893feaa7a8d830dbfc6b8b872
Gabriel <72566626+autowert66@users.noreply.github.com>
2025-06-14 22:52:44 +0200

    🪲 improve reliability of bot spawn detection

--- src/plugins/playerNearNotificationPlugin.ts
+++ src/plugins/playerNearNotificationPlugin.ts
@@ -1,5 +1,5 @@
 import type { Plugin as BotPlugin } from 'mineflayer';
-import type { Vec3 } from 'vec3';
+import { Vec3 } from 'vec3';
 
 import { publishNotification } from '../util/notifications';
 
@@ -8,13 +8,15 @@ const recentNotifications = new Map<string, number>();
 
 export const playerNearNotificationPlugin: BotPlugin = (bot) => {
   // TODO: make this a seperate plugin,
-  //       should also work if the bot didnt't die already
   let botBaseLocation: Vec3;
-  bot.once('mainServer', () => {
-    bot.once('spawn', () => {
-      console.log('got bot spawn location');
-      botBaseLocation = bot.entity.position;
-    });
+  bot.once('death', () => {
+    bot._client.once('respawn', (packet, meta) => {
+      bot._client.once('position', (packet, meta) => {
+        console.log('got bot spawn position after death and respawn');
+        const { x, y, z } = packet;
+        botBaseLocation = new Vec3(x, y, z);
+      });
+    })
   });
 
   bot.on('entitySpawn', async (entity) => {
@@ -22,11 +24,11 @@ export const playerNearNotificationPlugin: BotPlugin = (bot) => {
 
     if (entity === bot.entity) return;
 
-    if (!botBaseLocation) return console.warn('player near, but bot spawn location is unknown');
+    if (!botBaseLocation) return console.warn('player near, but bot spawn location is unknown:', entity.username);
     if (bot.entity.position.distanceTo(botBaseLocation) > 100) return;
 
     const username = entity.username;
-    if(!username) return console.warn('player entity does not have a username');
+    if (!username) return console.warn('player entity does not have a username');
 
     const now = Date.now();
     if (recentNotifications.has(username)) {
